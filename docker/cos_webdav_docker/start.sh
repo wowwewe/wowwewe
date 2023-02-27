142bf6fe164c3bd904e29f555f36816ea8172ebc
wowaqly <60953666+wowaqly@users.noreply.github.com>
2023-02-27 14:05:14 +0800

    Update start.sh

--- docker/cos_webdav_docker/start.sh
+++ docker/cos_webdav_docker/start.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 service cron start && service cron reload && service cron restart
+rm -rf /root/webdavserver/webdav_config.yaml
+cat >/root/webdavserver/webdav_config.yaml <<EOF
+address: 0.0.0.0
+port: 8080
+auth: true
+tls: false
+prefix: /
+
+scope: /root/pwdbak
+modify: true
+rules: []
+
+cors:
+  enabled: true
+  credentials: true
+  allowed_headers:
+    - Depth
+  allowed_hosts:
+    - http://localhost:8080
+  allowed_methods:
+    - GET
+  exposed_headers:
+    - Content-Length
+    - Content-Range
+
+users:
+ - username: $DAV_USER
+   password: $DAV_PWD
+scope: /root/pwdbak
+modify: true
+EOF
 cd /root/webdavserver && ./webdav -c /root/webdavserver/webdav_config.yaml
