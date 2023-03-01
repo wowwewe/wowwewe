f52a8ec81e8c06f2b3054f489220c2328530c66e
wowaqly <60953666+wowaqly@users.noreply.github.com>
2023-03-01 14:26:00 +0800

    Update zipbak.sh

--- docker/cos_webdav_docker/zipbak.sh
+++ docker/cos_webdav_docker/zipbak.sh
@@ -3,6 +3,6 @@ then
     mkdir -p '/root/zipbak'
 fi
 cd /root/zipbak
-zip -q -r pwd-backup-"$(date +"%Y-%m-%d-%H-%M")" /root/pwdbak
+zip -q -r pwd-backup-"$(date +"%Y-%m-%d-%H-%M")" /root/data
 coscmd upload -rs /root/zipbak > /dev/null 2>&1
 rm -f /root/zipbak/*
