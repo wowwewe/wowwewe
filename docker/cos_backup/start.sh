860bc7fe6878127d6fcb5288399ef3e3dbbf55a4
wowaqly <60953666+wowaqly@users.noreply.github.com>
2023-03-04 21:03:24 +0800

    Update start.sh

--- docker/cos_backup/start.sh
+++ docker/cos_backup/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm -rf ~/.cos.conf && coscmd config -a ${SECRETID} -s ${SECRETKEY} -b ${BUCKETNAME_APPID} -r ${REGION}
 cron
-bash /root/zipbak.sh
+bash /root/sync.sh
+tail -F /root/runing.txt
