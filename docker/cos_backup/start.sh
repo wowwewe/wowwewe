4495df58612b6c5cbcd0d73d2049880d7a001fdc
wowaqly <60953666+wowaqly@users.noreply.github.com>
2023-03-04 21:19:37 +0800

    Update start.sh

--- docker/cos_backup/start.sh
+++ docker/cos_backup/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 rm -f /root/sync.sh
-echo '#!/bin/bash' > /root/sync.sh && echo 'coscmd upload -rs /root/data ${COS_PATH} > /dev/null 2>&1' >> /root/sync.sh
+cat > /root/sync.sh <<EOF
+#!/bin/bash
+coscmd upload -rs /root/data ${COS_PATH} > /dev/null 2>&1
+EOF
 rm -rf ~/.cos.conf && coscmd config -a ${SECRETID} -s ${SECRETKEY} -b ${BUCKETNAME_APPID} -r ${REGION}
 cron
 bash /root/sync.sh
