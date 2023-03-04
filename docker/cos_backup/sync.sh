ece0e13f01fca29b1b27cd0f7332542690aabdb2
wowaqly <60953666+wowaqly@users.noreply.github.com>
2023-03-04 21:03:52 +0800

    Update and rename zipbak.sh to sync.sh

---
+++ docker/cos_backup/sync.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+coscmd upload -rs /root/data / > /dev/null 2>&1
--- docker/cos_backup/zipbak.sh
+++
@@ -1,8 +0,0 @@
-if [ ! -d '/root/backup' ]
-then
-    mkdir -p '/root/backup'
-fi
-cd /root/backup
-zip -q -r -P $PASSWORD backup-"$(date +"%Y-%m-%d-%H-%M")"-"$PASSWORD_PROMPT".zip /root/data
-coscmd upload -rs /root/backup / > /dev/null 2>&1
-rm -f /root/backup/*
