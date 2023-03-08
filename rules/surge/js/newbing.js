19734935d4c4cf6e44093ac4286a844b025ee9a7
wowaqly <60953666+wowaqly@users.noreply.github.com>
2023-03-08 23:40:31 +0800

    Update newbing.js

--- rules/surge/js/newbing.js
+++ rules/surge/js/newbing.js
@@ -1,10 +1,10 @@
 // 获取请求头
 let header = $request.headers;
 
-// 修改 x-forwarded-for 参数值，如果失效替换其他国外ip尝试
+// 修改 x-forwarded-for 参数值 如果失效替换其他国外ip尝试
 header['x-forwarded-for'] = '8.8.8.8';
 
-// 修改 user-agent 参数值，如果失效去找最新的edge user-agent替换
+// 修改 user-agent 参数值 如果失效去找最新的edge user-agent替换
 header['user-agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.57';
 
 // 返回修改后的请求头
