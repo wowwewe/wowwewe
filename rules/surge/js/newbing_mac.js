5df0662966e9d10424604cbac6529bf9f9c7b4b6
wowaqly <60953666+wowaqly@users.noreply.github.com>
2023-03-09 09:32:42 +0800

    Update newbing_mac.js

--- rules/surge/js/newbing_mac.js
+++ rules/surge/js/newbing_mac.js
@@ -2,7 +2,7 @@
 let header = $request.headers;
 
 // 修改 x-forwarded-for 参数值 如果失效替换其他国外ip尝试
-header['x-forwarded-for'] = '8.8.8.8';
+header['x-forwarded-for'] = '212.192.15.55';
 
 // 修改 user-agent 参数值 如果失效去找最新的edge user-agent替换
 header['user-agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.57';
