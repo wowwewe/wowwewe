603f09ebe11673e0da75c094b91f1ad5f4bf663a
wowaqly <60953666+wowaqly@users.noreply.github.com>
2023-03-13 15:22:25 +0800

    Update newbing_ios.js

--- rules/surge/js/newbing_ios.js
+++ rules/surge/js/newbing_ios.js
@@ -1,4 +1,4 @@
 let header = $request.headers;
-header['x-forwarded-for'] = '212.192.15.55';
+header['x-forwarded-for'] = '104.28.195.185';
 header['user-agent'] = 'Mozilla/5.0 (Linux; Android 11; V2072A) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36 EdgA/112.0.1696.0';
 $done({headers: header});
