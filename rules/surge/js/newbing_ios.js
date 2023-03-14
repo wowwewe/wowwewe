98ecf7f62b0f7deb244059416cfe42d548c5e391
wowaqly <60953666+wowaqly@users.noreply.github.com>
2023-03-14 08:55:51 +0800

    Delete newbing_ios.js

--- rules/surge/js/newbing_ios.js
+++
@@ -1,4 +0,0 @@
-let header = $request.headers;
-header['x-forwarded-for'] = '104.28.195.185';
-header['user-agent'] = 'Mozilla/5.0 (Linux; Android 11; V2072A) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36 EdgA/112.0.1696.0';
-$done({headers: header});
