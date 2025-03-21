8176976606b3add99bf177713ac2acd96656ba14
wowwewe <60953666+wowwewe@users.noreply.github.com>
2025-03-21 10:35:33 +0800

    Create start.sh

---
+++ docker/wireguard_snell_docker/start.sh
@@ -0,0 +1,13 @@
+rm -f /root/snell-server.conf
+cat > /root/snell-server.conf <<EOF
+{
+[snell-server]
+listen = 0.0.0.0:${PORT}
+psk = ${PSK}
+ipv6 = true
+obfs = off
+dns = ${DNS}
+}
+EOF
+wg-quick up wg0
+cd /root && ./snell-server -c /root/snell-server.conf
